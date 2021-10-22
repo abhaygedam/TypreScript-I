5259b8750968cfe803369fef8295ef50e82a51cd
abhaygedam <abhaygedam1999@gmail.com>
2021-10-22 19:46:24 +0530

    Typescript-Intro I

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+let a = 1;
+a = 5;
+a = 10;
+console.log("hello");
+console.log(a);
---
+++ src/index.ts
@@ -0,0 +1,23 @@
+
+let a: number = 1;
+a = 10;
+
+let b: boolean = false;
+b = true;
+b = undefined;
+
+let c: null;
+c = null;
+
+let str: string = "abhay";
+
+let arr: Array<number> = [1, 2, 3, 4, 5];
+
+let arr2: string[] = ["a", "b", "c", "d", "e", "f"];
+
+let g: Record<string, string[]> = {
+    a: ["abhay"],
+    b: ["sagar"],
+    c: ["adarsh", "chandraj"],
+    d: ["amol", "chake"],   
+}
